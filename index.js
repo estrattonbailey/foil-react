e6fd359b7816cf74da8a8e5b6eb86cf451df9d05
estrattonbailey <ericstrattonbailey@gmail.com>
2018-02-01 12:37:13 -0500

    Adjust logic for v16

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export default function createRouter (resolver) {
       super (props)
 
       this.state = {
-        Comp: props.children[0]
+        Comp: props.children.pop ? props.children[0] : props.children
       }
 
       resolver(Comp => this.setState({ Comp }))
