11eca320a6ab744981fcfd1af071d4510402cc13
estrattonbailey <ericstrattonbailey@gmail.com>
2018-05-18 12:12:39 -0400

    Transpile spread operator until microbundle#39 is merged

--- Link.js
+++ Link.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import { store } from './history.js'
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-export function Link ({ href, children, className, activeLocation }) {
-  const cx = ((className || '') + (
-    activeLocation === href ? ' active' : ''
-  )).replace(/^\s|\s\s/g, '')
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-  return (
-    <a href={href} className={cx} onClick={e => {
-      if (
-        e.ctrlKey ||
-        e.metaKey ||
-        e.altKey ||
-        e.shiftKey ||
-        e.defaultPrevented
-      ) return
+import React from 'react';
+import { store } from './history.js';
 
-      e.preventDefault()
+export function Link(_ref) {
+  let { href, children, className, activeLocation } = _ref,
+      props = _objectWithoutProperties(_ref, ['href', 'children', 'className', 'activeLocation']);
 
-      store.hydrate({
-        location: href.replace(window.location.origin, '')
-      })()
-    }}>{children}</a>
-  )
+  const cx = ((className || '') + (activeLocation === href ? ' active' : '')).replace(/^\s|\s\s/g, '');
+
+  return React.createElement(
+    'a',
+    _extends({ href: href, className: cx, onClick: e => {
+        if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.defaultPrevented) return;
+
+        e.preventDefault();
+
+        store.hydrate({
+          location: href.replace(window.location.origin, '')
+        })();
+      } }, props),
+    children
+  );
 }
