1ceb0a4511e7f21b9f0abfe4c4f1a268e0e58133
estrattonbailey <ericstrattonbailey@gmail.com>
2018-07-30 10:26:06 -0400

    Improve link parsing

--- Link.js
+++ Link.js
@@ -5,7 +5,7 @@ import { pick } from './util.js'
 export const Link = withHistory(
   function Link (props) {
     const { picked, rest } = pick(props, [
-      'children', 'href', 'className', 'history'
+      'children', 'href', 'className', 'history', 'target', 'download'
     ])
 
     const { location, pathname } = picked.history.state
@@ -17,6 +17,10 @@ export const Link = withHistory(
       ) ? ' active' : ''
     )).replace(/^\s|\s\s/g, '')
 
+    const opts = {}
+    if (picked.target) opts.target = picked.target
+    if (picked.download) opts.download = picked.download
+
     return (
       <a href={picked.href} className={cx} onClick={e => {
         if (
@@ -24,13 +28,17 @@ export const Link = withHistory(
           e.metaKey ||
           e.altKey ||
           e.shiftKey ||
-          e.defaultPrevented
+          e.defaultPrevented ||
+          opts.target === '_blank' ||
+          opts.download ||
+          /mailto|tel/.test(picked.href) ||
+          /^(https?:)?\/\//.test(picked.href)
         ) return
 
         e.preventDefault()
 
         picked.history.push(picked.href)
-      }} {...rest}>{picked.children}</a>
+      }} {...opts} {...rest}>{picked.children}</a>
     )
   }
 )
