96f101e5910c649722931249707666824ceff867
estrattonbailey <ericstrattonbailey@gmail.com>
2019-05-06 10:36:07 -0400

    handle user-defined onClick events

--- Link.js
+++ Link.js
@@ -4,25 +4,34 @@ import { pick } from './util.js'
 
 export const Link = withHistory(
   function Link (props) {
-    const { picked, rest } = pick(props, [
-      'children', 'href', 'className', 'history', 'target', 'download'
-    ])
+    const {
+      children,
+      href,
+      className,
+      history,
+      target,
+      download,
+      onClick,
+      ...rest
+    } = props
 
-    const { location, pathname } = picked.history.state
+    const { location, pathname } = history.state
 
-    const cx = ((picked.className || '') + (
+    const cx = ((className || '') + (
       (
-        location === picked.href ||
-        pathname === picked.href
+        location === href ||
+        pathname === href
       ) ? ' active' : ''
     )).replace(/^\s|\s\s/g, '')
 
     const opts = {}
-    if (picked.target) opts.target = picked.target
-    if (picked.download) opts.download = picked.download
+    if (target) opts.target = target
+    if (download) opts.download = download
 
     return (
-      <a href={picked.href} className={cx} onClick={e => {
+      <a href={href} className={cx} onClick={e => {
+        onClick && onClick(e)
+
         if (
           e.ctrlKey ||
           e.metaKey ||
@@ -31,14 +40,14 @@ export const Link = withHistory(
           e.defaultPrevented ||
           opts.target === '_blank' ||
           opts.download ||
-          /mailto|tel/.test(picked.href) ||
-          /^(https?:)?\/\//.test(picked.href)
+          /mailto|tel/.test(href) ||
+          /^(https?:)?\/\//.test(href)
         ) return
 
         e.preventDefault()
 
-        picked.history.push(picked.href)
-      }} {...opts} {...rest}>{picked.children}</a>
+        history.push(href)
+      }} {...opts} {...rest}>{children}</a>
     )
   }
 )
